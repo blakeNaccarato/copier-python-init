f04aa06da76e7f7e9a7fae41c7cd1e693d2e8008
blakeNaccarato <blake.naccarato@gmail.com>
2022-04-16 14:57:30 -0700

    Don't merge gitignore in init step

--- template/.tooling/scripts/merge_gitignore.ps1
+++
@@ -1,9 +0,0 @@
-$original = Get-Content -Delimiter \0 -Path .gitignore
-$replace = Get-Content -Delimiter \0 -Path .tooling/scripts/gitignore_to_merge
-
-$lookbehind = '(?<=\s#\[\[\s)'  # "#[[\n" (have to use \s instead of \n)
-$match = '[\s\S]*'  # anything in-between
-$lookahead = '(?=\s#\]\]\s)'  # "#]]\n" (have to use \s instead of \n)
-
-$original -replace "$lookbehind$match$lookahead", $replace |
-    Set-Content -NoNewline -Path .gitignore
