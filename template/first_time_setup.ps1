11b33ac423181a9bd8ca3aa1b265c77bc97ea83a
blakeNaccarato <blake.naccarato@gmail.com>
2023-01-02 17:07:12 -0800

    Self destruct after running

--- template/first_time_setup.ps1
+++ template/first_time_setup.ps1
@@ -2,3 +2,4 @@ git submodule add https://github.com/blakeNaccarato/copier-python.git template
 git submodule add https://github.com/blakeNaccarato/pylance-stubs-unofficial.git typings
 copier copy template .
 git submodule deinit --all
+Remove-Item first_time_setup.ps1
