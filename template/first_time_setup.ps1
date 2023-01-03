ec62f1f5b722ff526f63ec4e15117e74a3af54c6
blakeNaccarato <blake.naccarato@gmail.com>
2023-01-02 17:24:40 -0800

    Copier can't diff on submodule templates. Just get the commit then.

--- template/first_time_setup.ps1
+++ template/first_time_setup.ps1
@@ -1,5 +1,5 @@
 git submodule add https://github.com/blakeNaccarato/copier-python.git template
 git submodule add https://github.com/blakeNaccarato/pylance-stubs-unofficial.git typings
-copier copy template .
+copier -f -r $(git rev-parse HEAD:template)
 git submodule deinit --all
 Remove-Item first_time_setup.ps1
