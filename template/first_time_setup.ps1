3b8fb78e978e6b17137027958f02dd14a4174b47
blakeNaccarato <blake.naccarato@gmail.com>
2023-04-27 16:14:12 -0700

    Commit initial changes in setup

--- template/first_time_setup.ps1
+++ template/first_time_setup.ps1
@@ -1,10 +1,11 @@
 Remove-Item first_time_setup.ps1
 git add -A
-git commit -m "Initialize template"
+git commit -m "Prepare template using blakeNaccarato/copier-python-init"
 git submodule add https://github.com/blakeNaccarato/copier-python.git template
 git submodule add https://github.com/blakeNaccarato/pylance-stubs-unofficial.git typings
 git add -A
-git commit -m "Add submodules"
+git commit -m "Add template and type stub submodules"
 git submodule deinit --all
 copier gh:blakeNaccarato/copier-python . -f
 git add -A
+git commit -m "Initialize template using blakeNaccarato/copier-python"
